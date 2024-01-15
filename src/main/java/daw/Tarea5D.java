66b998ea37479c869433749eb1762660e830099e
ralcara908 <ralcara908@g.educaand.es>
2024-01-15 09:41:23 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/daw/Tarea5D.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author rocio
+ */
+public class Tarea5D {
+    int numero;
+    int paginas;
+}
