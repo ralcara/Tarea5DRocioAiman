db65bc22b3acb8397437634b1b19b74e9bd457f9
Aiman <ahardao1001@g.educaand.es>
2024-01-15 09:48:42 +0100

    constructores , getters y setters , tostring

--- src/main/java/daw/Tarea5D.java
+++ src/main/java/daw/Tarea5D.java
@@ -9,7 +9,29 @@ package daw;
  * @author rocio
  */
 public class Tarea5D {
-    int numero;
-    int paginas;
+    private int numero;
+    private int paginas;
     //pagina
+
+    public Tarea5D(int numero, int paginas) {
+        this.numero = numero;
+        this.paginas = paginas;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
 }
