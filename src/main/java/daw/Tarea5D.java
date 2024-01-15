b408e08e303247327ca4f715bea0cfb1964c4edb
Aiman <ahardao1001@g.educaand.es>
2024-01-15 10:01:30 +0100

    c

--- src/main/java/daw/Tarea5D.java
+++ src/main/java/daw/Tarea5D.java
@@ -13,11 +13,16 @@ public class Tarea5D {
     private int paginas;
     //pagina
 
+    //constructor 
     public Tarea5D(int numero, int paginas) {
         this.numero = numero;
         this.paginas = paginas;
     }
+    //constructor por defecto 
 
+    public Tarea5D() {
+    }
+    
     public int getPaginas() {
         return paginas;
     }
@@ -32,6 +37,16 @@ public class Tarea5D {
 
     public void setNumero(int numero) {
         this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tarea5D{");
+        sb.append("numero=").append(numero);
+        sb.append(", paginas=").append(paginas);
+        sb.append('}');
+        return sb.toString();
     }
     
 }
